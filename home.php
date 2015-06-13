7ad92a47c6e23c5ba6c384f0da41d61c2227175b
gruebait <gruebait@eatenbygrues.com>
2015-06-13 17:00:52 -0400

    Introduce home page template.
    Add a placeholder home page template, using home.php instead of
    front-page.php, per
    https://developer.wordpress.org/themes/basics/template-hierarchy/#home-page-display.

---
+++ home.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * The template file for the home page.
+ *
+ * See: https://developer.wordpress.org/themes/basics/template-hierarchy/#home-page-display
+ *      https://codex.wordpress.org/Creating_a_Static_Front_Page
+ *
+ * @package Zorkish
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php /* Start the Loop */ ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php
+
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content', get_post_format() );
+				?>
+
+			<?php endwhile; ?>
+
+			<?php the_posts_navigation(); ?>
+
+		<?php else : ?>
+
+			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
