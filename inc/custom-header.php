f0c79a43af2f58bf867e9011a25a8790865cca77
gruebait <gruebait@eatenbygrues.com>
2015-06-14 12:01:38 -0400

    Add preliminary custom header support.

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -25,8 +25,8 @@ function zorkish_custom_header_setup() {
 	add_theme_support( 'custom-header', apply_filters( 'zorkish_custom_header_args', array(
 		'default-image'          => '',
 		'default-text-color'     => '000000',
-		'width'                  => 1000,
-		'height'                 => 250,
+		'width'                  => 1600,
+		'height'                 => 1200,
 		'flex-height'            => true,
 		'wp-head-callback'       => 'zorkish_header_style',
 		'admin-head-callback'    => 'zorkish_admin_header_style',
