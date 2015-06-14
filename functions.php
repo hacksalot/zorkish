0cd859ed2d2fc28be596aa45fc5981fd4e17b638
gruebait <gruebait@eatenbygrues.com>
2015-06-14 19:49:12 -0400

    Add description for primary sidebar widget.

--- functions.php
+++ functions.php
@@ -86,7 +86,7 @@ function zorkish_widgets_init() {
   register_sidebar( array(
     'name'          => esc_html__( 'Sidebar', 'zorkish' ),
     'id'            => 'sidebar-1',
-    'description'   => '',
+    'description'   => 'The primary site sidebar.',
     'before_widget' => '<aside id="%1$s" class="widget %2$s">',
     'after_widget'  => '</aside>',
     'before_title'  => '<h1 class="widget-title">',
