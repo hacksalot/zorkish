8ca16ec0087ac77326ebaf4d50132f4f8d5d5d2e
gruebait <gruebait@eatenbygrues.com>
2015-06-14 14:08:10 -0400

    Remove broken infinite scroll support.
    Left over from Underscores's buggy implementation.

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -16,20 +16,20 @@ function zorkish_jetpack_setup()
     'header-text' => array('site-title','site-description'),
     'size' => 'site-logo' 
   )); 
-  add_theme_support( 'infinite-scroll', array(
-    'container' => 'main',
-    'render'    => 'zorkish_infinite_scroll_render',
-    'footer'    => 'page',
-  ) );
+  // add_theme_support( 'infinite-scroll', array(
+    // 'container' => 'main',
+    // 'render'    => 'zorkish_infinite_scroll_render',
+    // 'footer'    => 'page',
+  // ) );
 }
 add_action( 'after_setup_theme', 'zorkish_jetpack_setup' );
 
-/**
-Custom render function for Infinite Scroll.
-*/
-function zorkish_infinite_scroll_render() {
-  while ( have_posts() ) {
-    the_post();
-    get_template_part( 'template-parts/content', get_post_format() );
-  }
-} // end function zorkish_infinite_scroll_render
+// /**
+// Custom render function for Infinite Scroll.
+// */
+// function zorkish_infinite_scroll_render() {
+  // while ( have_posts() ) {
+    // the_post();
+    // get_template_part( 'template-parts/content', get_post_format() );
+  // }
+// } // end function zorkish_infinite_scroll_render
