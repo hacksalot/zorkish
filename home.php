8374a2271cc509bb0ab33341ac774b96b4c7b121
gruebait <gruebait@eatenbygrues.com>
2015-06-14 14:46:01 -0400

    Remove post navigation links from home page.

--- home.php
+++ home.php
@@ -12,25 +12,16 @@ get_header(); ?>
     <main id="main" class="site-main" role="main">
 
     <?php if ( have_posts() ) : ?>
-
       <?php /* Start the Loop */ ?>
       <?php while ( have_posts() ) : the_post(); ?>
-
-        <?php
-          // Include the Post-Format-specific template for the content.
+        <?// Include the Post-Format-specific template for the content.
           // If you want to override this in a child theme, then include a file
           // called content-___.php (where ___ is the Post Format name) and that will be used instead.
           get_template_part( 'template-parts/content', get_post_format() );
         ?>
-
       <?php endwhile; ?>
-
-      <?php the_posts_navigation(); ?>
-
     <?php else : ?>
-
       <?php get_template_part( 'template-parts/content', 'none' ); ?>
-
     <?php endif; ?>
 
     </main><!-- #main -->
