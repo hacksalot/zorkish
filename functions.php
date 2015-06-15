003cbbba528a5805f326eac4c016d4d46fe55f5c
gruebait <gruebait@eatenbygrues.com>
2015-06-15 12:26:11 -0400

    Introduce footer widget container.
    Support elevator pitches and other suffix content in in the site footer.

--- functions.php
+++ functions.php
@@ -91,7 +91,16 @@ function zorkish_widgets_init() {
     'after_widget'  => '</aside>',
     'before_title'  => '<h1 class="widget-title">',
     'after_title'   => '</h1>',
-  ) );
+  ));
+  register_sidebar( array(
+    'name'          => esc_html__( 'Footer', 'zorkish' ),
+    'id'            => 'sidebar-2',
+    'description'   => 'The footer bar.',
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget'  => '</aside>',
+    'before_title'  => '<h1 class="widget-title">',
+    'after_title'   => '</h1>',
+  ));
 }
 add_action( 'widgets_init', 'zorkish_widgets_init' );
 
