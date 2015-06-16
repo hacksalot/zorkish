bc39ad8fb699ff64eda0317d8370f8e3ced51245
gruebait <gruebait@eatenbygrues.com>
2015-06-16 10:09:30 -0400

    Strip title margins on home/archive pages.

--- template-parts/content.php
+++ template-parts/content.php
@@ -9,7 +9,7 @@ Template part for displaying posts.
   <header class="entry-header">
     <?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 
-    <?php if ( 'post' == get_post_type() ) : ?>
+    <?php if ( 'post' == get_post_type() && is_single() ) : ?>
     <div class="entry-meta">
       <?php zorkish_posted_on(); ?>
     </div><!-- .entry-meta -->
