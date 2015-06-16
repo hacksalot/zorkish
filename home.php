4a251a3548de6d6328c7db027c7c83d3186d51c5
gruebait <gruebait@eatenbygrues.com>
2015-06-16 12:07:07 -0400

    Scrub home.php.
    Remove unnecessary tags from starter theme.

--- home.php
+++ home.php
@@ -11,19 +11,18 @@ get_header(); ?>
   <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
 
-    <?php if ( have_posts() ) : ?>
-      <?php /* Start the Loop */ ?>
-      <?php while ( have_posts() ) : the_post(); ?>
-        <?// Include the Post-Format-specific template for the content.
-          // If you want to override this in a child theme, then include a file
-          // called content-___.php (where ___ is the Post Format name) and that will be used instead.
-          get_template_part( 'template-parts/content', get_post_format() );
-        ?>
-      <?php endwhile; ?>
-    <?php else : ?>
-      <?php get_template_part( 'template-parts/content', 'none' ); ?>
-    <?php endif; ?>
-
+    <?php
+    if ( have_posts() ) :
+      while ( have_posts() ) : the_post();
+        // Include the Post-Format-specific template for the content.
+        // If you want to override this in a child theme, then include a file
+        // called content-___.php (where ___ is the Post Format name) and that will be used instead.
+        get_template_part( 'template-parts/content', get_post_format() );
+      endwhile;
+    else :
+      get_template_part( 'template-parts/content', 'none' );
+    endif;
+    ?>
     </main><!-- #main -->
   </div><!-- #primary -->
 
